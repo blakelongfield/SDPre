d556857731c3e4b66a1a321c435e525229eed400
Blake Longfield <blakelongfield@gmail.com>
2018-08-21 19:55:23 -0600

    complete Objective2Lab5

---
+++ Labs/Objective2Lab5.java
@@ -0,0 +1,14 @@
+public class Objective2Lab5 {
+  public static void main(String[] args) {
+    double num = Math.sqrt(3 * 3);   //num =>3
+    double result = Math.sqrt(4) + Math.sqrt(9); //result => 2 + 3 = >5
+    double side1, side2, hypotenuse;
+
+    side1 = 10.0;
+    side2 = 8.0;
+    hypotenuse = (side1 * side1) + (side2 * side2);
+    hypotenuse = Math.sqrt(hypotenuse);
+
+    System.out.println("The hypotenuse of a trianle with sides " + side1 + " and " + side2 + " is " + hypotenuse);
+  }
+}
