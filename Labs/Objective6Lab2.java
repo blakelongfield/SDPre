305d2c0296c5c9c363c08b3561f3fc17a6f4e7cf
Blake Longfield <blakelongfield@gmail.com>
2018-08-22 16:21:31 -0600

    completed Objective6Lab2

---
+++ Labs/Objective6Lab2.java
@@ -0,0 +1,13 @@
+public class Objective6Lab2 {
+  public static void main(String[] args) {
+  int counter = 0;
+
+  while(counter <= 20) {
+    System.out.println(counter);
+    counter = counter + 1; //to make sure counter adds by 1
+  }
+    if (counter == 20) {
+      System.out.println(counter);
+    }
+  }
+}
